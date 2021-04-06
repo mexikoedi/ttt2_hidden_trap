6768ca0771f9e348cbee38543b7d49d1b5883e7a
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-04-06 21:54:52 +0200

    initial upload

---
+++ ttt2_hidden_trap/lua/terrortown/lang/de/hiddentrap.lua
@@ -0,0 +1,6 @@
+L = LANG.GetLanguageTableReference("de")
+L["ttt2_hidden_trap_name"] = "Versteckte Falle"
+L["ttt2_hidden_trap_desc"] = "Platziere eine unsichtbare und gefährliche Falle für deine Gegner. Du und alle aus deinem Team sind dagegen immun."
+L["ttt2_hidden_trap_help1"] = "{primaryfire} Schießen Sie auf eine Oberfläche, um die Falle zu platzieren."
+L["ttt2_hidden_trap_help2"] = "{secondaryfire} Spielen Sie einen zufälligen Ton ab."
+L["ttt2_hidden_trap_popuptitle"] = "Huh, was war das?"
