2547aa97c7468b04e15906ade870ac154563560b
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-05-11 18:18:29 +0200

    renamed convar

--- ttt2_hidden_trap/gamemodes/terrortown/entities/entities/ent_ttt2_hidden_trap/cl_init.lua
+++ ttt2_hidden_trap/gamemodes/terrortown/entities/entities/ent_ttt2_hidden_trap/cl_init.lua
@@ -7,5 +7,5 @@ net.Receive("ttt2_hidden_trap_epop", function()
     EPOP:AddMessage({
         text = LANG.GetTranslation("ttt2_hidden_trap_popuptitle"),
         color = Color(255, 204, 203, 255)
-    }, "", GetConVar("ttt2_hidden_trap_popup_duration"):GetInt())
+    }, "", GetConVar("ttt2_hidden_trap_damage_popup_duration"):GetInt())
 end)
