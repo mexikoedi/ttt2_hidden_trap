eb2225d703397fa1411c0acf2e83f2d6381beb89
mexikoedi <mexikoedi@users.noreply.github.com>
2021-12-31 16:02:35 +0100

    small lang update

--- lua/terrortown/lang/de/hiddentrap.lua
+++ lua/terrortown/lang/de/hiddentrap.lua
@@ -1,4 +1,4 @@
-L = LANG.GetLanguageTableReference("de")
+local L = LANG.GetLanguageTableReference("de")
 L["ttt2_hidden_trap_name"] = "Versteckte Falle"
 L["ttt2_hidden_trap_desc"] = "Platziere eine unsichtbare und gefährliche Falle für deine Gegner. Du und alle aus deinem Team sind dagegen immun."
 L["ttt2_hidden_trap_help1"] = "{primaryfire} Schießen Sie auf eine Oberfläche, um die Falle zu platzieren."
